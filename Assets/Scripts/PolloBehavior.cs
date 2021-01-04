265f4962246d9dbd08175992d3fc9761fd835707
Andrea <andrea.rosati87@yahoo.it>
2021-01-04 06:22:00 +0100

    Esplosionje fatta, manca audio

--- Assets/Scripts/PolloBehavior.cs
+++ Assets/Scripts/PolloBehavior.cs
@@ -7,6 +7,9 @@ public class PolloBehavior : MonoBehaviour
     private float mass;
     public GameObject polletto;
     public GameObject pulcino;
+    private float timeofbirth;
+
+    private float timeToLive = 4.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +20,57 @@ public class PolloBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeToLive -= Time.deltaTime;
         
+
+        if(timeToLive < 0)
+            Destroy(this.gameObject);
+
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        print("UN POLLOOOOOO" + mass);
-        Destroy(this.gameObject);
+
+        if (mass > 9)
+        {
+            generateChild(polletto);
+            generateChild(polletto);
+            Destroy(this.gameObject);
+            return;
+        }
 
 
+     if (mass > 4)
+        {
+                if (timeToLive > 3.8)
+                    return;
+
+            generateChild(pulcino);
+            generateChild(pulcino);
+            Destroy(this.gameObject);
+            return;
+        }
+     
+  
     }
+
+
+    private void generateChild(GameObject kind)
+    {
+        Vector3 oldPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 oldVelocity = this.GetComponent<Rigidbody>().velocity;
+        oldPosition.Set(oldPosition.x, oldPosition.y, oldPosition.z);
+
+        //oldPosition.normalized
+
+        GameObject polletto1 = Instantiate(kind,
+                               oldPosition,
+                               transform.rotation);
+        Vector3 polletto1Velocity = new Vector3(oldVelocity.normalized.x, oldVelocity.normalized.y, oldVelocity.normalized.z);
+        polletto1Velocity.Scale(new Vector3(Random.value < .5 ? 1 : -1, Random.value < .5 ? 1 : -1, -1));
+        polletto1.GetComponent<Rigidbody>().AddForce(polletto1Velocity * Random.Range(25, 48), ForceMode.Impulse);
+    }
+
+
 }
--- Assets/Scripts/TankBehavior.cs
+++ Assets/Scripts/TankBehavior.cs
@@ -8,28 +8,63 @@ public class TankBehavior : MonoBehaviour
     public GameObject bullet;
     private float speed = 160f;
     public Transform shootPosition;
+    private AudioSource audioData;
+    private float nextShoot;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        audioData = GetComponent<AudioSource>();
+        nextShoot = RandomGaussian(1.8f, 6.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        nextShoot -= Time.deltaTime;
+        if (nextShoot < 0)
         {
-            GameObject instantiatedPollo = Instantiate(bullet,
-                                                           shootPosition.position,
-                                                           shootPosition.rotation);
-
-            instantiatedPollo.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed, ForceMode.Impulse);
-
+            nextShoot = RandomGaussian(3, 8);
+            shoot();
         }
     }
 
-    
-    
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void shoot()
+    {
+        audioData.Play(0);
+        GameObject instantiatedPollo = Instantiate(bullet,
+                                                       shootPosition.position,
+                                                       shootPosition.rotation);
+
+        instantiatedPollo.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 1) * speed, ForceMode.Impulse);
+    }
+
+
+    public static float RandomGaussian(float minValue = 0.0f, float maxValue = 1.0f)
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * UnityEngine.Random.value - 1.0f;
+            v = 2.0f * UnityEngine.Random.value - 1.0f;
+            S = u * u + v * v;
+        }
+        while (S >= 1.0f);
+
+        // Standard Normal Distribution
+        float std = u * Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+
+        // Normal Distribution centered between the min and max value
+        // and clamped following the "three-sigma rule"
+        float mean = (minValue + maxValue) / 2.0f;
+        float sigma = (maxValue - mean) / 3.0f;
+        return Mathf.Clamp(std * sigma + mean, minValue, maxValue);
+    }
+
 }
